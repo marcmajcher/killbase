c73445a81a9ba4df0c8776ce0a480f5dd32cfb21
Ryder Dale <rydersdale@gmail.com>
2018-06-20 14:00:30 -0500

    reordered create tables so that references will work

--- killbase.sql
+++ killbase.sql
@@ -3,11 +3,6 @@ CREATE DATABASE killbase;
 \c killbase; 
 
 CREATE TABLE Assassins (assassin_id serial primary key, name text, rating float, check(rating BETWEEN 0 AND 10), weapon_of_choice text, minimum_fee integer, age integer, kills integer);
-CREATE TABLE Targets (target_id serial, PRIMARY KEY (target_id), name text, location text, photo text, security_level text);
-CREATE TABLE Clients (client_id serial, PRIMARY KEY (client_id), name text);
-CREATE TABLE Contracts (contract_id serial, PRIMARY KEY (contract_id), target integer references Targets(target_id), client integer references Clients (client_id), budget integer, complete_status boolean, by_who references Assassins (assassin_id));
-CREATE TABLE Assassins_Contracts (assassin references Assassins (assassin_id), contract references Contracts (contract_id));
-CREATE TABLE Assassin_Codenames (assassin references Assassins (assassin_id), codename text); 
 
 INSERT INTO Assassins (name, weapon_of_choice, age, minimum_fee, rating, kills) VALUES 
 ('Alexander Duggan', 'Sniper rifle', '31', '45', '7.5', '28'),
@@ -21,6 +16,8 @@ INSERT INTO Assassins (name, weapon_of_choice, age, minimum_fee, rating, kills) 
 INSERT INTO Assassins (name, weapon_of_choice, age, minimum_fee, rating, kills) VALUES ('Nikita Mears', 'Silenced pistols', '28', '30', '7', '32');
 INSERT INTO Assassins (name, weapon_of_choice, age, minimum_fee, rating, kills) VALUES ('Pickle Rick', 'Lasers and office supplies', '60', '0', '8', '24');
 
+CREATE TABLE Assassin_Codenames (assassin references Assassins (assassin_id), codename text); 
+
 INSERT INTO Codenames (assassin, codename) VALUES ('1', 'The Jackal');
 INSERT INTO Codenames (assassin, codename) VALUES ('2', 'Old Man');
 INSERT INTO Codenames (assassin, codename) VALUES ('3', 'Ghost Dog');
@@ -29,11 +26,17 @@ INSERT INTO Codenames (assassin, codename) VALUES ('7', 'The Professional');
 INSERT INTO Codenames (assassin, codename) VALUES ('8', 'Nikita, La Femme Nikita');
 INSERT INTO Codenames (assassin, codename) VALUES ('9', 'Solenya');
 
+
+CREATE TABLE Targets (target_id serial, PRIMARY KEY (target_id), name text, location text, photo text, security_level text);
+
 INSERT INTO Targets (name, location, photo, securitylevel) VALUES ('Butch Coolidge','Los Angeles','https://goo.gl/LCquZj','3');
 INSERT INTO Targets (name, location, photo, securitylevel) VALUES ('The Jaguar','Russian Embassy','https://goo.gl/6JWsiv','9');
 INSERT INTO Targets (name, location, photo, securitylevel) VALUES ('Norman Stansfield','Manhattan','https://i.imgur.com/mdIk33E.jpg','7');
 INSERT INTO Targets (name, location, photo, securitylevel) VALUES ('Santino D''Antonio','Continental Hotel','https://goo.gl/fUPkYy','10');
 INSERT INTO Targets (name, location, photo, securitylevel) VALUES ('Sonny Valerio','Queens','https://goo.gl/8DHYUS','4');
+
+
+CREATE TABLE Clients (client_id serial, PRIMARY KEY (client_id), name text);
 
 INSERT INTO Clients (name) VALUES ('Marcellus Wallace');
 INSERT INTO Clients (name) VALUES ('Concerto');
@@ -41,8 +44,21 @@ INSERT INTO Clients (name) VALUES ('Mathilda');
 INSERT INTO Clients (name) VALUES ('Winston');
 INSERT INTO Clients (name) VALUES ('Ray Vargo');
 
+
+CREATE TABLE Assassins_Contracts (assassin references Assassins (assassin_id), contract references Contracts (contract_id));
+
+
+CREATE TABLE Contracts (contract_id serial, PRIMARY KEY (contract_id), target integer references Targets(target_id), client integer references Clients (client_id), budget integer, complete_status boolean, by_who references Assassins (assassin_id));
+
 INSERT INTO contracts (target, client, budget) VALUES ('1','1','40');
 INSERT INTO contracts (target, client, budget) VALUES ('2','2','70');
 INSERT INTO contracts (target, client, budget) VALUES ('3','3','35');
 INSERT INTO contracts (target, client, budget) VALUES ('4','4','25');
 INSERT INTO contracts (target, client, budget) VALUES ('5','5','10');
+
+
+
+
+
+
+
